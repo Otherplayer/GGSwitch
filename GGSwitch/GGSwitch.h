e041d089d5bf454d14b78be3fff77e64b5c824e3
zero <osmk@qq.com>
2015-04-30 18:55:26 +0800

    build

---
+++ GGSwitch/GGSwitch.h
@@ -0,0 +1,13 @@
+//
+//  GGSwitch.h
+//  GGSwitch
+//
+//  Created by __无邪_ on 15/4/29.
+//  Copyright (c) 2015年 __无邪_. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GGSwitch : UIView
+
+@end
---
+++ GGSwitch/GGSwitch.m
@@ -0,0 +1,21 @@
+//
+//  GGSwitch.m
+//  GGSwitch
+//
+//  Created by __无邪_ on 15/4/29.
+//  Copyright (c) 2015年 __无邪_. All rights reserved.
+//
+
+#import "GGSwitch.h"
+
+@implementation GGSwitch
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
